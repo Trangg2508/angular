1339304e3de49a89bc15d27eb717ca432d4a3d76
Trang <ngotrang2508.forwork@gmail.com>
2023-07-27 10:21:51 +0700

    ngForApDung

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,16 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit{
-public name = 'Trang';
-public age = 21;
-public fruits = ['Apple', 'Grapes', 'Orange', 'Watermelon', 'Guava', 'Papaya', 'Durian'];
+export class HomeComponent implements OnInit {
+  public name = 'Trang';
+  public age = 21;
+  public fruits = ['Apple', 'Grapes', 'Orange', 'Watermelon', 'Guava', 'Papaya', 'Durian'];
+  public fruit2 = [
+    { name: 'Apple', price: 12, sale: true },
+    { name: 'Banana', price: 10, sale: false },
+    { name: 'Grapes', price: -5, sale: true }
+  ];
 
-constructor() {};
+  constructor() { };
 
-public ngOnInit(): void {
-  console.log('Fruit = ',this.fruits);
-}
+  public ngOnInit(): void {
+    console.log('Fruit = ', this.fruits);
+  }
 
 
 }
